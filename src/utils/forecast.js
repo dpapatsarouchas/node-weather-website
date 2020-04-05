d67e991f72a67eabc167e96eaa1cc65243170113
Dimitris Papatsarouchas <d.papatsarouchas@gmail.com>
2020-04-05 23:07:07 +0300

    Add weather forecast inforamtions

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,12 +12,38 @@ const forecast = (lat,long,callback) => {
             callback(body.error, undefined);
         } else {
             let cw = body.currently;
+            let todayWeather = body.daily.data[1];
             let temperature = cw.temperature;
             let rain_chance = cw.precipProbability;
+            let humidity = cw.humidity;
+            let higherTemperature = todayWeather.temperatureHigh;
+            let lowerTemperature = todayWeather.temperatureLow;
 
-            callback(undefined,`${body.daily.data[0].summary} Η θερμοκρασία είναι ${temperature} βαθμούς κελισίου. Υπάρχει ${rain_chance}% πιθανότητα βροχής`)
+            callback(undefined,`${body.daily.data[0].summary} Η θερμοκρασία είναι ${temperature} βαθμοί κελισίου.
+            Υψηλότερη θερμοκρασία ${higherTemperature} βαθμοί κελισίου.
+            Χαμηλότερη θερμοκρασία ${lowerTemperature} βαθμοί κελισίου.
+            Υπάρχει ${Math.round(rain_chance*100)}% πιθανότητα βροχής.
+            Ποσοστό θερμοκρασίας ${humidity}.`)
         }
     });
 };
 
-module.exports = forecast;
+const testForecast = (lat, long) => {
+    const url = `https://api.darksky.net/forecast/fef9c7578d4f17f9225535b691cc9f09/${lat},${long}?lang=el&units=si`;
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            console.error(error);
+        } else if (body.error) {
+            console.error(body.error);
+        } else {
+            console.log(body.currently);
+        }
+    });
+};
+
+if (require.main === module) {
+    testForecast('37.96667', '23.61667');
+} else {
+    module.exports = forecast;
+}
